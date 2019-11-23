f403e71bd69c43b53d5e95dfb8d21b79c78cd5ce
yy163 <y405096069@163.com>
2019-11-23 11:28:15 +0800

    zyy111

---
+++ xzb-zhglpt-sys/src/main/java/com/nfdw/mapper/SpecManagementMapper.java
@@ -0,0 +1,16 @@
+package com.nfdw.mapper;
+
+import com.nfdw.base.BaseMapper;
+import com.nfdw.entity.SpecCollect;
+import com.nfdw.entity.SpecManagement;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SpecManagementMapper extends BaseMapper<SpecManagement,String> {
+
+    List<SpecCollect> selectSpecCollect();
+
+    SpecCollect selectIdByName(@Param("collect_id") String id);
+
+}
--- xzb-zhglpt-sys/src/main/java/com/nfdw/service/impl/SpecManagementMapper.java
+++
@@ -1,4 +0,0 @@
-package com.nfdw.service.impl;
-
-public class SpecManagementMapper {
-}
